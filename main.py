8c80a4c17d3bf28234be1cc0ee69e6cf2784e595
Rory Milsom <s2731136@ed.ac.uk>
2025-12-02 20:06:17 +0000

    Intitial Imports

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn as sb
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn import metrics
+from xgboost import XGBClassifier
+import warnings
+warnings.filterwarnings('ignore')
+
